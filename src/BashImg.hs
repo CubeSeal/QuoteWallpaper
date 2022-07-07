9e4daf2fabe1c0ea365c20be17f2c9089e8ff726
landseal <kenndesilva1@gmail.com>
2022-07-07 20:52:49 +1000

    Changed working dir to .quotewallpaper and added ReaderT for working dir.

--- src/BashImg.hs
+++ src/BashImg.hs
@@ -8,29 +8,43 @@ module BashImg
 -- Modules
 import System.Process (callProcess, readProcess)
 import UsefulFunctions ( getISODate )
-import Control.Monad.Reader ()
+import Control.Monad.Reader (ReaderT, MonadReader (ask), MonadIO (liftIO))
 
 import qualified Clippings as C
   ( Quote ( Quote
           , author
           , book
           , quote ) )
-import qualified WikimediaAPI as W ( URL )
+import qualified WikimediaAPI as W ( URL, fetchPOTD )
 import qualified Data.Text.Lazy as T ( unpack, fromStrict )
 import qualified System.Directory as D (doesFileExist)
 
+createImageFile :: C.Quote -> ReaderT FilePath IO FilePath
+createImageFile quote = do
+  url <- liftIO W.fetchPOTD
+  rawImgFilePath  <- downloadImageFile url
+  makeImageFile rawImgFilePath quote
+
 -- Set Wallpaper
-setWallpaper :: FilePath -> IO ()
-setWallpaper fp = callProcess "plasma-apply-wallpaperimage" [fp]
+setWallpaper :: FilePath -> ReaderT FilePath IO ()
+setWallpaper fp = do
+  dir <- ask
+  let picDir = dir ++ fp
+  liftIO $ callProcess "plasma-apply-wallpaperimage" [picDir]
 
 -- Make image file
-createImageFile :: C.Quote -> IO FilePath
-createImageFile C.Quote { C.author = a
-                        , C.book   = b
-                        , C.quote  = q } = do
-  formatedQuote <- readProcess "fold" ["-s"] $ T.unpack q
-  date          <- getISODate
-  let picDir   = "/home/landseal/Documents/QuoteWallpaper/" ++ date ++ ".jpg"
+makeImageFile :: FilePath -> C.Quote -> ReaderT FilePath IO FilePath
+makeImageFile
+  inImgFile
+  C.Quote { C.author = a
+          , C.book   = b
+          , C.quote  = q
+          } = do
+  formatedQuote <- liftIO $ readProcess "fold" ["-s"] $ T.unpack q
+  dir <- ask
+  let picDir   = dir ++ inImgFile
+      outFile  = "out.jpg"
+      outDir   = dir ++ outFile
       printStr = formatedQuote
         <> "\n\n\t— "
         <> T.unpack a
@@ -38,7 +52,8 @@ createImageFile C.Quote { C.author = a
         <> T.unpack b
         <> ")"
         <> "\n"
-  callProcess "convert"
+  liftIO $ callProcess
+    "convert"
     [ "-gravity"
     , "center"
     , "-resize"
@@ -55,20 +70,25 @@ createImageFile C.Quote { C.author = a
     , "+0+0"
     , printStr
     , picDir
-    , outDir ]
-  return outDir
-  where
-    outDir = "/home/landseal/Documents/QuoteWallpaper/out.jpg"
+    , outDir
+    ]
+  return outFile
 
 -- Download image file
-downloadImageFile :: W.URL -> IO ()
+downloadImageFile :: W.URL -> ReaderT FilePath IO FilePath
 downloadImageFile url = do
-  date <- getISODate
-  ind  <- D.doesFileExist $ "/home/landseal/Documents/QuoteWallpaper/" ++ date ++ ".jpg"
-  if ind
+  date <- liftIO getISODate
+  dir  <- ask
+  let imgFile = date ++ ".jpg"
+      fullPathImgFile = dir ++ imgFile
+  doesRawImgFileExist  <- liftIO $ D.doesFileExist fullPathImgFile
+  if doesRawImgFileExist
     then
-      return ()
-    else
-      callProcess "wget"
-        [ "--output-document=/home/landseal/Documents/QuoteWallpaper/" ++ date ++ ".jpg"
-        , (T.unpack . T.fromStrict) url ]
+      return imgFile
+    else do
+      liftIO $ callProcess
+        "wget"
+        [ "--output-document=" ++ fullPathImgFile
+        , (T.unpack . T.fromStrict) url
+        ]
+      return imgFile
--- src/Quotewallpaper.hs
+++ src/Quotewallpaper.hs
@@ -4,23 +4,37 @@
 module Quotewallpaper where
 
 -- Modules
-import qualified Clippings as C ( rawToQuotes )
+import Control.Monad.Reader (ReaderT(runReaderT))
+
+import qualified Clippings as C ( rawToQuotes, Quote )
 import qualified BashImg as B
   ( createImageFile
   , setWallpaper
-  , downloadImageFile )
-import qualified WikimediaAPI as W (fetchPOTD)
+  )
 import qualified Data.Text.Lazy as T
 import qualified System.Random as R (uniformR, initStdGen)
+import qualified System.Directory as D (getAppUserDataDirectory, createDirectoryIfMissing)
 
 main :: IO ()
 main = do
-  quotes <- C.rawToQuotes . T.pack <$> readFile "/home/landseal/Documents/QuoteWallpaper/My Clippings.txt"
+  dir    <- substantiateDir "quotewallpaper/"
+  quotes <- C.rawToQuotes
+    . T.pack
+    <$> readFile (dir ++ "My Clippings.txt")
+  ranQuote <- getRanQuote quotes
+  flip runReaderT dir $ do
+    imgFile <- B.createImageFile ranQuote
+    B.setWallpaper imgFile
+
+substantiateDir :: FilePath -> IO FilePath
+substantiateDir dirname = do
+  appDir <- D.getAppUserDataDirectory dirname
+  D.createDirectoryIfMissing True appDir
+  return appDir
+
+getRanQuote :: [C.Quote] -> IO C.Quote
+getRanQuote quotes = do
   stdGen <- R.initStdGen
   let (ranNum, _) = R.uniformR (0, length quotes - 1) stdGen
   -- This should be safe.
-  let ranQuote = quotes !! ranNum
-  url <- W.fetchPOTD
-  B.downloadImageFile url
-  imgFile <- B.createImageFile ranQuote
-  B.setWallpaper imgFile
+  return $ quotes !! ranNum
