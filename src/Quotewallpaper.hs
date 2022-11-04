2354a83fdf8a74b91fe4bf2ea9bd133aec674f19
landseal <kenndesilva1@gmail.com>
2022-11-04 18:00:32 +1100

    Added handling for missing clippings file.

--- src/Quotewallpaper.hs
+++ src/Quotewallpaper.hs
@@ -6,6 +6,7 @@ module Quotewallpaper where
 -- Modules
 import Control.Monad.Reader (ReaderT(runReaderT))
 import Data.Maybe (fromMaybe)
+import System.Exit (exitSuccess)
 
 import UsefulFunctions ((!?))
 
@@ -21,21 +22,39 @@ import qualified Data.Time as CL
 
 main :: IO ()
 main = do
+  -- Substantiate directory and check for kindle quotes file.
   dir    <- substantiateDir "quotewallpaper/"
+  checkMyClippingsExists dir
+  
+  -- Import and parse qutoes, and then get random one.
   quotes <- C.rawToAQuotes
     . T.pack
     <$> readFile (dir ++ "My Clippings.txt")
   ranQuote <- getRanQuote quotes
+
+  -- Download POTD file, add ran quote and set wallpaper.
   flip runReaderT dir $ do
     imgFile <- CMD.createImageFile ranQuote
     CMD.setWallpaper imgFile
 
+-- Create directory if not existing already.
 substantiateDir :: FilePath -> IO FilePath
 substantiateDir dirname = do
   appDir <- D.getAppUserDataDirectory dirname
   D.createDirectoryIfMissing True appDir
   return appDir
 
+-- Check for "My Clippings.txt" file and exit if not there.
+checkMyClippingsExists :: FilePath -> IO ()
+checkMyClippingsExists dir = do
+  p <- D.doesFileExist $ dir ++ "My Clippings.txt"
+  if p
+    then return ()
+    else do
+      putStrLn $ "Add My Clipping.txt to " ++ dir
+      exitSuccess
+
+-- Get Random Quote based on day and year.
 getRanQuote :: [C.AnnotatedQuote] -> IO C.AnnotatedQuote
 getRanQuote quotes = do
   (yearNum, dayNum) <- DT.toOrdinalDate . CL.utctDay <$> CL.getCurrentTime
