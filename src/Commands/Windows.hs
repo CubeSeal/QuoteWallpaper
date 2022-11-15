5832bfb747134accdce6d54dd3188a802c642ff6
Kenneth de Silva <kenndesilva1@gmail.com>
2022-11-15 21:48:38 +1100

    Added win32 lib.

--- src/Commands/Windows.hs
+++ src/Commands/Windows.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Commands.Windows
   ( setWallpaper
@@ -10,6 +12,7 @@ import Control.Monad.Reader ( ReaderT )
 import qualified Clippings as C
 import qualified Commands.Common as COM
 import qualified WikimediaAPI as W
+import Foreign.C (CString)
 
 createImageFile :: C.AnnotatedQuote -> W.URL -> ReaderT FilePath IO FilePath
 createImageFile quote url = do
@@ -21,3 +24,5 @@ makeImageFile inImgFile C.AQuote {..} = undefined
 
 setWallpaper :: FilePath -> ReaderT FilePath IO ()
 setWallpaper = undefined
+
+foreign import ccall "windows.h SetWallpaper" printF :: CString -> IO ()
