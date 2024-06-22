ec3a4c95a24b737af21c5461c69d3e77a24603cf
landseal <kenndesilva1@gmail.com>
2024-06-22 13:37:19 +1000

    Added drop shadow

--- src/Commands/KDE.hs
+++ src/Commands/KDE.hs
@@ -7,13 +7,13 @@ module Commands.KDE
   ) where
 
 -- Modules
-import System.Process (callProcess)
+-- Modules
+import System.Process (callProcess, callCommand)
 import Control.Monad.Reader
   ( ReaderT
   , MonadReader (ask)
   , MonadIO (liftIO)
   )
-import Data.Maybe (fromMaybe)
 
 import qualified Data.Text.Lazy as T
 
@@ -29,7 +29,7 @@ setWallpaper fp = do
   liftIO $ callProcess "plasma-apply-wallpaperimage" [picDir]
 
 -- | Make image file
-createImageFile 
+createImageFile
   :: MonadIO m
   => FilePath
   -> C.AnnotatedQuote
@@ -38,46 +38,71 @@ createImageFile inImgFile C.AQuote {..} = do
   Env dir _ <- ask
   date <- getISODate
   let
+    infixl 5 <|>
+    (<|>) x y = x <> " " <> y
     picDir   = dir ++ "infiles/" ++ inImgFile
     outFile  = "out-" ++ date ++ ".jpg"
     outDir   = dir ++ "outfiles/" ++ outFile
-    quoteStr = T.unpack aQuote
-      ++ "\n\n— "
-      ++ T.unpack aAuthor
-      ++ " ("
-      ++ T.unpack aBook
-      ++ ")"
-      ++ "\n"
-    noteStr  = T.unpack $ fromMaybe "" aNote
-  liftIO $ callProcess
+    quoteStr = T.unpack $
+      aQuote
+      <> "\n\n— "
+      <> T.replace "\"" "\\\"" aAuthor
+      <> " ("
+      <> aBook
+      <> ")"
+      <> "\n"
+    noteStr = T.unpack $
+      case aNote of
+        Nothing -> ""
+        Just text -> "-gravity southeast -annotate +100+100" <|> text
+  liftIO $ callCommand $
     "convert"
-    [ "-gravity"
-    , "center"
-    , "-resize"
-    , "1920x1080^"
-    , "-extent"
-    , "1920x1080"
-    , "+sigmoidal-contrast"
-    , "3x0%"
-    , "-blur"
-    , "0x0"
-    , "-font"
-    , "EB-Garamond-12-Regular"
-    , "-fill"
-    , "white"
-    , "-pointsize"
-    , "50"
-    , "-annotate"
-    , "+0+0"
-    , quoteStr
-    , "-font"
-    , "EB-Garamond-08-Italic"
-    , "-gravity"
-    , "southeast"
-    , "-annotate"
-    , "+100+100"
-    , noteStr
-    , picDir
-    , outDir
-    ]
+    <|> picDir
+    <|> "-gravity"
+    <|> "center"
+    <|> "-resize"
+    <|> "1920x1080^"
+    <|> "-extent"
+    <|> "1920x1080"
+    <|> "+sigmoidal-contrast"
+    <|> "3x0%"
+
+    <|> "\\("
+    <|> "-size"
+    <|> "1920x1080"
+    <|> "xc:transparent"
+    <|> "-font"
+    <|> "\"EB-Garamond-12-Regular\""
+    <|> "-pointsize"
+    <|> "50"
+    <|> "-fill"
+    <|> "black"
+    <|> "-draw"
+    <|> "\"text"
+    <|> "0,0"
+    <|> ("\'" <> quoteStr <> "\'" <> "\"")
+    <|> "-blur"
+    <|> "0x4"
+    <|> "\\)"
+    <|> "-geometry +5+5 -composite"
+
+    <|> "\\("
+    <|> "-blur"
+    <|> "0x0"
+    <|> "-font"
+    <|> "EB-Garamond-12-Regular"
+    <|> "-fill"
+    <|> "white"
+    <|> "-pointsize"
+    <|> "50"
+    <|> "-annotate"
+    <|> "+0+0"
+    <|> ("\'" <> quoteStr <> "\'")
+    <|> "\\)"
+
+    <|> "-font"
+    <|> "EB-Garamond-08-Italic"
+    <|> noteStr
+
+    <|> outDir
   return outFile
