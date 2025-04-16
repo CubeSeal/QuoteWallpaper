60154fb3c968dac403a4888cb017de0b84a20acd
Kenneth de Silva <24647624+CubeSeal@users.noreply.github.com>
2025-04-16 23:20:59 +1000

    Make monadic filter expression style.

--- src/Quotewallpaper.hs
+++ src/Quotewallpaper.hs
@@ -73,11 +73,17 @@ filterAQuote C.AQuote
   { C.aAuthor = a
   , C.aQuote = q
   , C.aDateTime = DT.LocalTime day _
-  } = (&&) <$> p4 <*> pure (p1 && p2 && p3)
-  where
+  } = do
+  today <- getDay
+  let
     p1 = maybe False (isPunctuation . snd) $ T.unsnoc q
     p2 = all (`notElem` T.words a) ["Kenneth", "Fred"]
     p3 = length (T.words q) > 1
-    p4 = do
-      today <- getDay
-      return $ today `DT.diffDays` day <= 1000
+    p4 = today `DT.diffDays` day <= 1000
+  
+  pure $ and
+    [ p1
+    , p2
+    , p3
+    , p4
+    ]
