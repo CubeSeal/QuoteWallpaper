19e8d64b7392fbf2a6b5f0d8ec2091d66eb42773
landseal <kenndesilva1@gmail.com>
2025-04-25 18:03:10 +1000

    Reformatting dalledownload.

--- src/DalleDownload.hs
+++ src/DalleDownload.hs
@@ -26,13 +26,13 @@ import GHC.Generics (Generic)
 import Data.Maybe (fromMaybe)
 import Data.Vector ((!?))
 
-import App (ApiKey (fromApiKey))
-
 import qualified Data.Text.Lazy as T
 import qualified Data.Aeson as H
 import qualified Data.Aeson.KeyMap as H
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString as BS
+
+import App (ApiKey (fromApiKey))
 
 import qualified Clippings as C
 
@@ -41,13 +41,7 @@ type Bytes = BS.ByteString
 
 -- | Quality of the image.
 data Quality = Low | Medium | High
-    deriving (Generic)
-
--- | Manual show instance for lower case prints.
-instance Show Quality where
-  show Low = "low"
-  show Medium = "medium"
-  show High = "high"
+    deriving (Show)
 
 instance H.ToJSON Quality where
     toJSON Low = H.String "low"
@@ -62,11 +56,11 @@ instance H.FromJSON Quality where
 
 -- | JSON format OpenAi API wants to receive info in.
 data OpenAiJsonQuery = OpenAiJsonQuery
-  { model :: T.Text
-  , prompt :: T.Text
-  , n :: Int
-  , size :: T.Text
-  , quality :: Quality
+  { model :: !T.Text
+  , prompt :: !T.Text
+  , n :: !Int
+  , size :: !T.Text
+  , quality :: !Quality
   } deriving (Show, Generic)
 
 instance H.ToJSON OpenAiJsonQuery
@@ -75,39 +69,36 @@ instance H.FromJSON OpenAiJsonQuery
 -- Functions
 -- | Generate Dalle3 Image and return URL
 fetchDalle3 :: MonadIO m => C.AnnotatedQuote -> ApiKey -> m Bytes
-fetchDalle3 C.AQuote {..} apiKey =
-  runReq defaultHttpConfig $ do
-    let
-      infixl 5 <|>
-      (<|>) x y = x <> " " <> y
-      bearerMsg = "Bearer " <> (encodeUtf8 . T.toStrict . fromApiKey) apiKey
-      commentaryStr (Just str) = " And my note:" <|> str
-      commentaryStr _ = mempty
-      testMessage = OpenAiJsonQuery
-        "gpt-image-1"
-        ( "Draw a picture inspired by the following quote:"
-        <|> "\""
-        <|> aQuote
-        <|> "\"."
-        <|> "Please think carefully and take your time with generating this image."
-        <|> "Make sure you don't accidentally put the text of the quote in the picture."
-        <|> "Ensure that your picture fully embodies the meaning of the quote in terms of"
-        <|> " artstyle and themes, taking into account subtext and hidden meaning."
-        <|> "To help you I'll give you the author as well:"
-        <|> aAuthor
-        <|> ", the text which the quote is sourced from:"
-        <|> aBook
-        <|> commentaryStr aNote
-        )
-        1
-        "1024x1024"
-        Medium
-      url = https "api.openai.com" /: "v1" /: "images" /: "generations"
-      params = header "Content-Type" "application/json"
-        <> header "Authorization" bearerMsg
-
+fetchDalle3 C.AQuote {..} apiKey = runReq defaultHttpConfig $ do
     _jsonResponse <- req POST url (ReqBodyJson testMessage) jsonResponse params
     return $ fromMaybe undefined $ extractURL $ responseBody _jsonResponse
+  where
+    infixl 5 <|>
+    (<|>) x y = x <> " " <> y
+    commentaryStr (Just str) = " And my note:" <|> str
+    commentaryStr _ = mempty
+    bearerMsg = "Bearer " <> (encodeUtf8 . T.toStrict . fromApiKey) apiKey
+    testMessage = OpenAiJsonQuery
+      "gpt-image-1"
+      ( "Draw a picture inspired by the following quote:"
+      <|> "\""
+      <|> aQuote
+      <|> "\"."
+      <|> "Please think carefully and take your time with generating this image."
+      <|> "Make sure you don't accidentally put the text of the quote in the picture."
+      <|> "Ensure that your picture fully embodies the meaning of the quote in terms of"
+      <|> " artstyle and themes, taking into account subtext and hidden meaning."
+      <|> "To help you I'll give you the author as well:"
+      <|> aAuthor
+      <|> ", the text which the quote is sourced from:"
+      <|> aBook
+      <|> commentaryStr aNote
+      )
+      1
+      "1024x1024"
+      Medium
+    url = https "api.openai.com" /: "v1" /: "images" /: "generations"
+    params = header "Content-Type" "application/json" <> header "Authorization" bearerMsg
 
 extractURL :: H.Value -> Maybe Bytes
 extractURL val = do
